d2aa4e8ed0437273f44fbe1004430d466a54f507
Jesper Kristensen <cableman@linuxdev.dk>
2021-02-10 22:19:33 +0100

    SERV-379: Updated app_dev.php to match new domains

--- web/app_dev.php
+++ web/app_dev.php
@@ -15,7 +15,7 @@ if (
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
     || php_sapi_name() === 'cli-server')
-  ) && $_SERVER['HTTP_HOST'] !== 'aaplusdata.vm'
+  ) && ($_SERVER['HTTP_HOST'] !== 'aaplus.local.itkdev.dk' || $_SERVER['HTTP_HOST'] !== 'stg.aaplusdata.dk')
 ) {
   header('HTTP/1.0 403 Forbidden');
   exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
